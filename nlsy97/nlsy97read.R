0e122361fb569c1845e5d91110c2f034ecf1a30f
Angie Shin <angieshin0701@gmail.com>
2021-02-07 09:56:52 -0800

    initial

---
+++ nlsy97/nlsy97read.R
@@ -0,0 +1,19 @@
+# clear workspace
+rm(list = ls())
+
+# load packages
+library(tidyverse)
+library(haven)
+
+# read in NLSY97 data
+nlsy97 <- read_dta("nlsy97.dta") %>%
+  rename_all(str_to_lower)
+
+# extract variable labels
+nlsy97_varlabels <- nlsy97 %>%
+  map(~ attr(.x, "label")) %>% 
+  enframe() %>%
+  transmute(name, label = str_to_lower(value))
+
+# view variable labels
+View(nlsy97_varlabels)
