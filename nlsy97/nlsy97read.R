8ee06782c28be47ed44bb731c35f7383d787362f
Angie Shin <angieshin0701@gmail.com>
2021-04-30 16:08:38 -0700

    update

--- nlsy97/nlsy97read.R
+++ nlsy97/nlsy97read.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(haven)
 
 # read in NLSY97 data
-nlsy97 <- read_dta("nlsy97.dta") %>%
+nlsy97 <- read_dta("nlsy97/nlsy97.dta") %>%
   rename_all(str_to_lower)
 
 # extract variable labels
@@ -17,3 +17,4 @@ nlsy97_varlabels <- nlsy97 %>%
 
 # view variable labels
 View(nlsy97_varlabels)
+
